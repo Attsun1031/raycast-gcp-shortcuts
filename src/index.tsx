11eb1c46df619e4854a8e196c95e085828696f0c
Attsun1031 <atsumi.tatsuya@gmail.com>
2022-02-10 20:25:25 +0900

    Lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,51 @@
-import {ActionPanel, Detail, List, OpenInBrowserAction, PushAction, randomId, showToast, ToastStyle,} from "@raycast/api";
-import {useEffect, useRef, useState} from "react";
-import os from 'os'
-import * as path from 'path'
-import * as fs from 'fs'
-import {AbortError} from "node-fetch";
+import {
+  ActionPanel,
+  Detail,
+  List,
+  OpenInBrowserAction,
+  PushAction,
+  randomId,
+  showToast,
+  ToastStyle,
+} from "@raycast/api";
+import { useEffect, useRef, useState } from "react";
+import os from "os";
+import * as path from "path";
+import * as fs from "fs";
+import { AbortError } from "node-fetch";
 
-const projectsFileName = ".raycast-gcp-shortcuts"
-const servicesJsonFile = "assets/gcp-services.json"
-const startMessage = "## How to start GCP Project Search command\n\
+const projectsFileName = ".raycast-gcp-shortcuts";
+const servicesJsonFile = "assets/gcp-services.json";
+const startMessage =
+  '## How to start GCP Project Search command\n\
 Thank you for install!\n\n\
 To start, please create your projects file by the command below.\n\n\
 ```\n\
-gcloud projects list --format=\"value(projectId)\" --sort-by=projectId > ~/.raycast-gcp-shortcuts\n\
+gcloud projects list --format="value(projectId)" --sort-by=projectId > ~/.raycast-gcp-shortcuts\n\
 ```\n\n\
 (Make sure to install and setup `gcloud` command first.)\n\
-"
+';
 
 export default function Command() {
   const { state, search } = useSearch();
 
   if (state.isLoading || state.hasProjectFile) {
-      return (
-          <List isLoading={state.isLoading} onSearchTextChange={search} searchBarPlaceholder="Search by project name..." throttle>
-              <List.Section title="Results" subtitle={state.results.length + ""}>
-                  {state.results.map((searchResult) => (
-                      <SearchListItem key={searchResult.id} searchResult={searchResult} />
-                  ))}
-              </List.Section>
-          </List>
-      );
+    return (
+      <List
+        isLoading={state.isLoading}
+        onSearchTextChange={search}
+        searchBarPlaceholder="Search by project name..."
+        throttle
+      >
+        <List.Section title="Results" subtitle={state.results.length + ""}>
+          {state.results.map((searchResult) => (
+            <SearchListItem key={searchResult.id} searchResult={searchResult} />
+          ))}
+        </List.Section>
+      </List>
+    );
   } else {
-      return <Detail markdown={ startMessage } />
+    return <Detail markdown={startMessage} />;
   }
 }
 
@@ -41,7 +56,10 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
       actions={
         <ActionPanel>
           <ActionPanel.Section>
-            <PushAction title={searchResult.name} target={<SearchServices project={searchResult.name} />} />
+            <PushAction
+              title={searchResult.name}
+              target={<SearchServices project={searchResult.name} />}
+            />
           </ActionPanel.Section>
         </ActionPanel>
       }
@@ -50,38 +68,53 @@ function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
 }
 
 function SearchServices({ project }: { project: string }) {
-  const { state, search } = useServiceSearch(project)
+  const { state, search } = useServiceSearch(project);
 
   return (
-      <List onSearchTextChange={search} searchBarPlaceholder="Search by name..." throttle>
-        <List.Section title="Results" subtitle={state.results.length + ""}>
-          {state.results.map((searchResult) => (
-              <SearchServiceItem key={searchResult.id} searchResult={searchResult} />
-          ))}
-        </List.Section>
-      </List>
-  )
+    <List
+      onSearchTextChange={search}
+      searchBarPlaceholder="Search by name..."
+      throttle
+    >
+      <List.Section title="Results" subtitle={state.results.length + ""}>
+        {state.results.map((searchResult) => (
+          <SearchServiceItem
+            key={searchResult.id}
+            searchResult={searchResult}
+          />
+        ))}
+      </List.Section>
+    </List>
+  );
 }
 
 function SearchServiceItem({ searchResult }: { searchResult: SearchResult }) {
   return (
-      <List.Item
-          title={searchResult.name}
-          actions={
-            <ActionPanel>
-              <ActionPanel.Section>
-                <OpenInBrowserAction title="Open in Browser" url={searchResult.url} />
-              </ActionPanel.Section>
-            </ActionPanel>
-          }
-      />
+    <List.Item
+      title={searchResult.name}
+      actions={
+        <ActionPanel>
+          <ActionPanel.Section>
+            <OpenInBrowserAction
+              title="Open in Browser"
+              url={searchResult.url}
+            />
+          </ActionPanel.Section>
+        </ActionPanel>
+      }
+    />
   );
 }
 
 function useSearch() {
-  const [state, setState] = useState<SearchState>({ results: [], isLoading: true, selectedProject: null, hasProjectFile: false });
+  const [state, setState] = useState<SearchState>({
+    results: [],
+    isLoading: true,
+    selectedProject: null,
+    hasProjectFile: false,
+  });
   const cancelRef = useRef<AbortController | null>(null);
-  const projectsLoadResultAsync = loadProjects()
+  const projectsLoadResultAsync = loadProjects();
 
   useEffect(() => {
     search("");
@@ -98,14 +131,17 @@ function useSearch() {
         ...oldState,
         isLoading: true,
       }));
-      const projectsLoadResult = await projectsLoadResultAsync
+      const projectsLoadResult = await projectsLoadResultAsync;
       setState((oldState) => ({
-          ...oldState,
-          hasProjectFile: projectsLoadResult.hasProjectFile,
+        ...oldState,
+        hasProjectFile: projectsLoadResult.hasProjectFile,
       }));
       let results: SearchResult[] = [];
       if (projectsLoadResult.hasProjectFile) {
-        results = searchMatchedProjects(searchText, projectsLoadResult.projects);
+        results = searchMatchedProjects(
+          searchText,
+          projectsLoadResult.projects
+        );
       }
       setState((oldState) => ({
         ...oldState,
@@ -128,50 +164,68 @@ function useSearch() {
 }
 
 async function loadProjects(): Promise<ProjectLoadResult> {
-    const projectsFilePath = path.join(os.homedir(), projectsFileName)
-    if (fs.existsSync(projectsFilePath)) {
-        let content = await fs.promises.readFile(projectsFilePath, 'utf-8')
-        return {
-            projects: content.split(/\r?\n/),
-            hasProjectFile: true,
-        }
-    } else {
-        return Promise.resolve({
-            projects: [],
-            hasProjectFile: false
-        })
-    }
+  const projectsFilePath = path.join(os.homedir(), projectsFileName);
+  if (fs.existsSync(projectsFilePath)) {
+    const content = await fs.promises.readFile(projectsFilePath, "utf-8");
+    return {
+      projects: content.split(/\r?\n/),
+      hasProjectFile: true,
+    };
+  } else {
+    return Promise.resolve({
+      projects: [],
+      hasProjectFile: false,
+    });
+  }
 }
 
-function searchMatchedProjects(searchText: string, projects: Array<string>): SearchResult[] {
-  return projects.filter(v => {
-    return v.includes(searchText)
-  }).map(v => {
-    return {
-      id: randomId(),
-      name: v,
-      url: `https://console.cloud.google.com/home/dashboard?project=${v}`,
-    };
-  })
+function searchMatchedProjects(
+  searchText: string,
+  projects: Array<string>
+): SearchResult[] {
+  return projects
+    .filter((v) => {
+      return v.includes(searchText);
+    })
+    .map((v) => {
+      return {
+        id: randomId(),
+        name: v,
+        url: `https://console.cloud.google.com/home/dashboard?project=${v}`,
+      };
+    });
 }
 
-async function searchServices(searchText: string, project: string): Promise<SearchResult[]> {
-  const content = await fs.promises.readFile(path.join(__dirname, servicesJsonFile), 'utf-8')
-  const services: ServiceObject[] = JSON.parse(content)
-  const results = services.filter(s => {
-    return s.name.toLowerCase().includes(searchText.toLowerCase())
-  }).map(s => {
-    return {
-      id: randomId(),
-      name: s.name,
-      url: s.url.replace("{{PROJECT_ID}}", project)
-    };
-  })
+async function searchServices(
+  searchText: string,
+  project: string
+): Promise<SearchResult[]> {
+  const content = await fs.promises.readFile(
+    path.join(__dirname, servicesJsonFile),
+    "utf-8"
+  );
+  const services: ServiceObject[] = JSON.parse(content);
+  const results = services
+    .filter((s) => {
+      return s.name.toLowerCase().includes(searchText.toLowerCase());
+    })
+    .map((s) => {
+      return {
+        id: randomId(),
+        name: s.name,
+        url: s.url.replace("{{PROJECT_ID}}", project),
+      };
+    });
   return Promise.resolve(results);
 }
 
 function useServiceSearch(project: string) {
-  const [state, setState] = useState<SearchState>({ results: [], isLoading: true, selectedProject: null, hasProjectFile: true });
+  const [state, setState] = useState<SearchState>({
+    results: [],
+    isLoading: true,
+    selectedProject: null,
+    hasProjectFile: true,
+  });
   const cancelRef = useRef<AbortController | null>(null);
 
   useEffect(() => {
@@ -229,6 +283,6 @@ interface ServiceObject {
 }
 
 interface ProjectLoadResult {
-    projects: string[];
-    hasProjectFile: boolean;
+  projects: string[];
+  hasProjectFile: boolean;
 }
